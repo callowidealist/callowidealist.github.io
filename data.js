7411cf96f5022951d23312038431dbe2583388e1
Jitendra Kumar Tripathi <9270669+callowidealist@users.noreply.github.com>
2021-01-31 16:59:33 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,48 +1,48 @@
 const main = {
-  name: 'Your Name',
-  mail: 'your.mail@gmail.com',
-  img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+  name: 'Jitendra Kumar Tripathi',
+  mail: 'jitendrakt2010@gmail.com',
+ // img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
   role: ['Full Stack Developer'],
   // Refer https://ionicons.com/ and put the icon name inside iconName key
   connects: [
     {
       name: 'Home',
       iconName: 'logo-firefox',
-      link: 'https://sivanesh-s.github.io'
+      link: 'https://callowidealist.github.io'
     },
     {
       name: 'Github',
       iconName: 'logo-github',
-      link: 'https://github.com/sivanesh-s'
+      link: 'https://github.com/callowidealist'
     },
     {
       name: 'Linkedin',
       iconName: 'logo-linkedin',
-      link: 'https://www.linkedin.com/in/sivanesh-shanmugam/'
+      link: 'https://www.linkedin.com/in/callowidealist/'
     },
     {
       name: 'StackOverflow',
       iconName: 'logo-stackoverflow',
-      link: 'https://stackoverflow.com/users/6940525/sivanesh-s'
+      link: 'https://stackoverflow.com/users/6940525/callowidealist'
     },
-    {
+    /*{
       name: 'Twitter',
       iconName: 'logo-twitter',
       link: 'https://twitter.com/sivanesh_fiz'
-    },
+    }, 
     {
       name: 'npm',
       iconName: 'logo-npm',
       link: 'https://www.npmjs.com/~sivanesh_s'
-    },
+    }, */
     {
       name: 'Mail',
       iconName: 'mail-outline',
-      link: `mailto:${'yourmail@gmail.com'}?Subject=Hello%20again`
+      link: `mailto:${'jitendrakt2010@gmail.com'}?Subject=Hello%20again`
     }
   ],
   links: [
     { name: 'Projects', link: '#projects' },
-    { name: 'Blogs', link: '#blogs' }
+    { name: 'Blogs', link: 'jitendra.hackernode.dev' }
   ]
 };
